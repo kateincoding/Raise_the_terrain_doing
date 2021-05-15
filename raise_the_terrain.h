c9822cdbb15a0d88f90a374a83f6fbdd25445ae7
kateincoding <kateincoding@gmail.com>
2021-05-15 10:42:49 -0500

    header until read_map

---
+++ raise_the_terrain.h
@@ -0,0 +1,28 @@
+#ifndef _RAISE_THE_TERRAIN_H_
+#define _RAISE_THE_TERRAIN_H_
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * SDL_Raise_the_terrain - Structure
+ * @window: hold the address of the window
+ * @renderer: renderer
+ */
+typedef struct SDL_Raise_the_terrain
+{
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+	unsigned int width;
+	unsigned int height;
+} sdl_rt;
+
+/* initialize program */
+int program_initialization(sdl_rt *map);
+/* read_map : get line and create the map size */
+int read_map(sdl_rt map, char *av);
+void draw_stuff(sdl_rt map);
+
+#endif /* raise_the_terrain_H */
